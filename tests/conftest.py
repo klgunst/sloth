cd07abfd60c539b64f2ee83e9be00004b906e766
Klaas Gunst <klaasgunst@hotmail.com>
2020-08-06 14:38:11 +0200

    Added tests for T3NS

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,3 @@
-from sloth.tensor import Tensor, Leg
-import sloth.symmetries as sls
-import itertools
-import numpy as np
-
-
-rng = np.random.default_rng()
-
-
-def make_random_l(nrirs, maxd):
-    """Make legs with random dimensions
-    """
-    return Leg(), [(k, rng.integers(0, maxd, endpoint=True))
-                   for k in itertools.product(*[range(x) for x in nrirs])]
-
-
-def make_random_t(symmetr, legs, flow):
-    """Make random tensors
-    """
-    A = Tensor(symmetr, [(l[0], f) for l, f in zip(legs, flow)])
-    for x in itertools.product(*[l[1] for l in legs]):
-        key = (tuple(y[0] for y in x),)
-        shape = tuple(y[1] for y in x)
-        if sls.is_allowed_coupling(key[0], flow, symmetr) and 0 not in shape:
-            A[key] = rng.uniform(low=-1.0, high=1.0, size=shape)
-    return A
-
-
 def pytest_addoption(parser):
     parser.addoption("--onlyspin", action="store_true",
                      help="run only for SU(2)-adapted tensors")
--- tests/test_entropies.py
+++ tests/test_entropies.py
@@ -6,8 +6,11 @@ import pytest
 import os
 
 
-with open('N2_svals.pickle', 'rb') as f:
-    checkvals = pickle.load(f)
+@pytest.fixture()
+def checkvals():
+    with open('N2_svals.pickle', 'rb') as f:
+        checkvals = pickle.load(f)
+    return checkvals
 
 
 def close_paddy(x, y, **kwargs):
@@ -18,24 +21,18 @@ def close_paddy(x, y, **kwargs):
     return np.allclose(*ar, **kwargs)
 
 
-@pytest.mark.parametrize("netwType", ["DMRG"])
-def test_singular_values(netwType):
+@pytest.mark.parametrize("netwType", ["DMRG", "T3NS"])
+def test_singular_values(netwType, kind, checkvals):
+    # Remove '(' and ')'
+    kind = kind.replace(')', '').replace('(', '')
     h5path = os.path.join(os.path.dirname(__file__), 'h5')
-    svals = {}
-
     # Obtained from the T3NS C implementation (github.com/klgunst/T3NS)
-    for kind in ['U1', 'SU2']:
-        tns = read_h5(os.path.join(h5path, f'N2_{kind}_{netwType}.h5'))
-        svals[kind] = {
-            tuple(tuple(sorted(xx)) for xx in tns.get_orbital_partition(x)):
-            flatten_svals(y) for x, y in
-            tns.calculate_singular_values(tns.sink).items()
-        }
-        # Check it is the same as check value
-        for x, y in checkvals[f'{kind}_{netwType}'].items():
-            assert close_paddy(y, svals[kind][x])  # similar svals
-
-    # Check that U1 is (approximately) the same as SU2
-    for x, y in svals['U1'].items():
-        assert np.isclose(np.linalg.norm(y), 1.)  # Normed tensor network
-        assert close_paddy(y, svals['SU2'][x], atol=1e-6)  # similar svals
+    tns = read_h5(os.path.join(h5path, f'N2_{kind}_{netwType}.h5'))
+    svals = {
+        tuple(tuple(sorted(xx)) for xx in tns.get_orbital_partition(x)):
+        flatten_svals(y) for x, y in
+        tns.calculate_singular_values(tns.sink).items()
+    }
+    # Check it is the same as check value
+    for x, y in checkvals[f'{kind}_{netwType}'].items():
+        assert close_paddy(y, svals[x])  # similar svals
